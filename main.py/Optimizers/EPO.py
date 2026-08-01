a1ac519b77dc35f3cc2c86a528b4c1d290e3502a
Shafi-B <shafibhuiyan2010@gmail.com>
2026-08-01 17:20:37 -0400

    Implement EPO optimizer using EPC algorithm

--- main.py/Optimizers/EPO.py
+++ main.py/Optimizers/EPO.py
@@ -1,1 +1,9 @@
+import math
+from mealpy import FloatVar
+from mealpy.swarm_based import EPC
 
+def EPO(cost_function):
+    problem_dict = {"bounds": FloatVar(lb=-math.pi, ub=math.pi, name="delta"), "obj_func": cost_function, "minmax": "min", "verbose": False, "log_to": "None"}
+    optimizer = EPC.DevEPC(epoch=1000, pop_size=50)
+    best = optimizer.solve(problem_dict)
+    return best
