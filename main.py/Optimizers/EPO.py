70bd1afe5029156889c0808da09efad81e66d360
Shafi-B <shafibhuiyan2010@gmail.com>
2026-08-01 23:18:38 -0400

    Update EPO optimizer parameters for bounds and settings

--- main.py/Optimizers/EPO.py
+++ main.py/Optimizers/EPO.py
@@ -1,9 +1,9 @@
-import math
 from mealpy import FloatVar
 from mealpy.swarm_based import EPC
 
+#Emperor Penguin Optimizer
 def EPO(cost_function):
-    problem_dict = {"bounds": FloatVar(lb=-math.pi, ub=math.pi, name="delta"), "obj_func": cost_function, "minmax": "min", "verbose": False, "log_to": "None"}
-    optimizer = EPC.DevEPC(epoch=1000, pop_size=50)
+    problem_dict = {"bounds": FloatVar(lb=-180, ub=180, name="delta"), "obj_func": cost_function, "minmax": "min", "verbose": False, "log_to": "None"}
+    optimizer = EPC.DevEPC(epoch=300, pop_size=10)
     best = optimizer.solve(problem_dict)
     return best
