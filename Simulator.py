94627f4d8d3900904ad066af640067d591acd3eb
Shafi-B <shafibhuiyan2010@gmail.com>
2026-08-01 23:15:57 -0400

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from scipy.sparse.linalg import eigsh
 
 path = Path(__file__).resolve().parent
 sys.path.append(str(path / "main.py"))
@@ -7,7 +8,26 @@ sys.path.append(str(path / "main.py" / "Optimizers"))
 
 import VQE
 from EPO import EPO
+from COBYLA import COBYLA
+from Nelder_Mead import Nelder_Mead
+from SLSQP import SLSQP
+from SPSA import SPSA
 
-result = EPO(Ansatz.expectation_value)
-print("Best angle:", result.solution[0])
-print("Lowest energy:", result.target.fitness)
+eigenvalues, eigenvectors = eigsh(VQE.molecule(), k=1, which="SA")
+print("Exact Solution Best Angle:", 12.8, "degrees")
+print("Exact Solution:", eigenvalues[0], "hartrees")
+EPO_result = EPO(VQE.expectation_value)
+print("EPO Best Angle:", EPO_result.solution[0], "degrees")
+print("EPO Lowest Energy:", EPO_result.target.fitness, "hartrees")
+COBYLA_result = COBYLA(VQE.expectation_value)
+print("COBYLA Best Angle:", COBYLA_result.x[0], "degrees")
+print("COBYLA Lowest Energy:", COBYLA_result.fun, "hartrees")
+Nelder_Mead_result = Nelder_Mead(VQE.expectation_value)
+print("Nelder-Mead Best Angle:", Nelder_Mead_result.x[0], "degrees")
+print("Nelder-Mead Lowest Energy:", Nelder_Mead_result.fun, "hartrees")
+SLSQP_result = SLSQP(VQE.expectation_value)
+print("SLSQP Best Angle:", SLSQP_result.x[0], "degrees")
+print("SLSQP Lowest Energy:", SLSQP_result.fun, "hartrees")
+SPSA_result = SPSA(VQE.expectation_value)
+print("SPSA Best Angle:", SPSA_result[0], "degrees")
+print("SPSA Lowest Energy:", VQE.expectation_value(SPSA_result), "hartrees")
